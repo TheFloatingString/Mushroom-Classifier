2d0b55a18e4576b750871a5859e0ee99a81ff592
Laurence <laurencel2001@gmail.com>
2018-06-03 14:49:55 -0400

    Add HTML file

--- flask_op/flask_backend.py
+++ flask_op/flask_backend.py
@@ -1,1 +1,13 @@
-import flask
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+
+def home():
+    return render_template('index.html')
+
+if __name__ == '__name__':
+    app.run(debug=True)
